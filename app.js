021e5185afbc35a225d6450fdfcfebbf8a34198f
Haoze <70860816+40022808@users.noreply.github.com>
2024-06-16 14:16:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import {
+    cat, start, start2,
+} from "./函数库.js";
+
+
+
+cat()
+
+start()
+start2()
+
+
---
+++ 函数库.js
@@ -0,0 +1,62 @@
+
+
+export function cat() {
+    console.log(`
+------------------------------------------------------------
+
+⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻⣿⣿⣿              
+⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿⣿⣿              
+⣿⣿⣿⡀⣿⣿⣿⣧⢻⣿⣶⣷⣿⣿⣿⣿⣿⣿⠿⠶⡝⠀⣿⣿⣿              
+⣿⣿⣿⣷⠘⣿⣿⣿⢏⣿⣿⣋⣀⣈⣻⣿⣿⣷⣤⣤⣿⡐⢿⣿⣿               
+⣿⣿⣿⣿⣆⢩⣝⣫⣾⣿⣿⣿⣿⡟⠿⠿⠦⠀⠸⠿⣻⣿⡄⢻⣿
+⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⠇⣼⣿       恭喜你发现了
+⣿⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰⣿⣿       彩蛋猫，只要  
+⣿⣿⣿⣿⣿⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣿⣿⣿       带它找到主人
+⣿⣿⣿⣿⣿⠏⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿       你就可以获得
+⣿⣿⣿⣿⠟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿⣿       神秘奖励!
+⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⣿⣿
+⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿
+
+------------------------------------------------------------
+`)
+
+}
+
+var name_code = document.querySelector('#name_code')
+
+
+
+export function start() {
+    const startbutton = document.querySelector('.startbutton')
+    const start = document.querySelector('.start')
+    const start2 = document.querySelector('.start2')
+    startbutton.addEventListener('click',()=>{
+        start.style.display = 'none';
+        start2.style.display = 'block';
+    })
+}
+
+
+export function start2() {
+    const startbutton = document.querySelector('.确定')
+    const start2 = document.querySelector('.start2')
+    const 游戏 = document.querySelector('.游戏')
+    startbutton.addEventListener('click',()=>{
+        默认名字()
+        个人信息_名字()
+        start2.style.display = 'none';
+        游戏.style.display = 'block';
+    })
+}
+
+function 默认名字() {
+    if (name_code.value == '') {
+        name_code.value = "[忘记输入自己名字的冒险者]"
+    }
+}
+
+
+function 个人信息_名字() {
+    const 名字显示 = document.querySelector('.名字显示')
+    名字显示.innerHTML += name_code.value
+}
