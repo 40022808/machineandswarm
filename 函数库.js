39f68d098ca2d326631a8fc94ddd4f3c6daaddc7
Haoze <70860816+40022808@users.noreply.github.com>
2024-06-17 17:00:39 +0200

    Add files via upload

--- 函数库.js
+++ 函数库.js
@@ -28,6 +28,8 @@ var me防御力 = 5
 var me体质 = 10
 var me精神 = 10
 var me等级 = 1
+var me经验 = 0
+var me经验上限 = 100
 var me生命上限 = me体质 * 10
 var me生命 = me生命上限
 var me魔力上限 = me精神 * 10
@@ -40,11 +42,24 @@ export function 属性刷新() {
     const 体质 = document.querySelector('.体质')
     const 精神 = document.querySelector('.精神')
     const 等级 = document.querySelector('.等级')
+    const 经验值 = document.querySelector('.经验值')
     攻击力.innerHTML += me攻击力
     防御力.innerHTML += me防御力
     体质.innerHTML += me体质
     精神.innerHTML += me精神
     等级.innerHTML += me等级
+    状态刷新()
+}
+
+function 状态刷新() {
+    const css生命 = document.querySelector('.css生命')
+    const css魔力 = document.querySelector('.css魔力')
+    let mehp = me生命 / me生命上限
+    let memaxhp = 1000
+    css生命.style.width = (memaxhp * mehp) + 'px'
+    let memp = me魔力 / me魔力上限
+    let memaxmp = 1000
+    css魔力.style.width = (memaxmp * memp) + 'px'
 }
 
 
@@ -53,9 +68,13 @@ export function start() {
     const startbutton = document.querySelector('.startbutton')
     const start = document.querySelector('.start')
     const start2 = document.querySelector('.start2')
+    const 悠闲 = document.querySelector('#悠闲')
+    
     startbutton.addEventListener('click',()=>{
         start.style.display = 'none';
         start2.style.display = 'block';
+        悠闲.volume = 0.2
+        悠闲.play()
     })
 }
 
@@ -64,11 +83,13 @@ export function start2() {
     const startbutton = document.querySelector('.确定')
     const start2 = document.querySelector('.start2')
     const 游戏 = document.querySelector('.游戏')
+    const 开局选项 = document.querySelector('.开局选项')
     startbutton.addEventListener('click',()=>{
         默认名字()
         个人信息_名字()
         start2.style.display = 'none';
         游戏.style.display = 'block';
+        开局选项.style.display = 'block';
     })
 }
 
